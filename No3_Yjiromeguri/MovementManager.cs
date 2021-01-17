a62ca67f2005923e6f8b823abe0e9d899e8534f2
ryosuke <averu.rec@gmail.com>
2021-01-17 17:06:14 +0900

    プロジェクト追加：No3_Yjiromeguri

---
+++ No3_Yjiromeguri/MovementManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yjiromeguri
+{
+    /// <summary>
+    /// 移動を管理するオブジェクト
+    /// </summary>
+    public class MovementManager
+    {
+        /// <summary>
+        /// ある接点に隣接する接点を、反時計回りに全て合わせた文字列を返すディクショナリ
+        /// </summary>
+        public static Dictionary<char, string> NextNode = new Dictionary<char, string>()
+        {
+            { 'A',"BCD" },
+            { 'B',"AEC" },
+            { 'C',"ABF" },
+            { 'D',"AFE" },
+            { 'E',"BDF" },
+            { 'F',"CED" },
+        };
+
+        #region フィールド／プロパティ
+
+        /// <summary>
+        /// 一つ前に止まっていた接点名
+        /// </summary>
+        private char _postNode;
+
+        /// <summary>
+        /// 現在止まっている接点名
+        /// </summary>
+        public char NowNode { get; private set; }
+
+        #endregion
+
+        #region コンストラクタ
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="fromDef">出発点(初期値)</param>
+        /// <param name="toDef">移動点(初期値)</param>
+        public MovementManager(char fromDef, char toDef)
+        {
+            _postNode = fromDef;
+            NowNode = toDef;
+        }
+
+        #endregion
+
+        #region メソッド
+
+        /// <summary>
+        /// 右折させる
+        /// </summary>
+        /// <remarks>
+        /// NextNodesにおいて、post点の文字から一つ次の文字が右折点
+        /// 例)NextNodesが"BCD"の場合、post点が'C'なら次の文字は'D', post点が'D'なら次の文字は'B'
+        /// </remarks>
+        public void TurnRight()
+        {
+            var postNodeTmp = NowNode;
+
+            var nextNodes = NextNode[NowNode];
+            var nextNodesNum = nextNodes.Count();
+            for (var i = 0; i < nextNodesNum; i++)
+            {
+                if (_postNode == nextNodes[i])
+                {
+                    // 次の文字を取り出す(終端の場合は最初の文字)
+                    NowNode = (i == nextNodesNum - 1) ? nextNodes[0]
+                                                      : nextNodes[i + 1];
+                }
+            }
+            _postNode = postNodeTmp;
+        }
+
+        /// <summary>
+        /// 左折させる
+        /// </summary>
+        /// <remarks>
+        /// NextNodesにおいて、post点の文字から一つ前の文字が左折点
+        /// 例)NextNodesが"ABF"の場合、post点が'F'なら前の文字は'B', post点が'A'なら前の文字は'F'
+        /// </remarks>
+        public void TurnLeft()
+        {
+            var postNodeTmp = NowNode;
+
+            var nextNodes = NextNode[NowNode];
+            var nextNodesNum = nextNodes.Count();
+            for (var i = 0; i < nextNodesNum; i++)
+            {
+                if (_postNode == nextNodes[i])
+                {
+                    // 前の文字を取り出す(始端の場合は最後の文字)
+                    NowNode = (i == 0) ? nextNodes[nextNodesNum -1]
+                                       : nextNodes[i - 1];
+                }
+            }
+            _postNode = postNodeTmp;
+        }
+
+        /// <summary>
+        /// 一つ元に戻る
+        /// </summary>
+        public void Back()
+        {
+            // nowとpostを入れ替える
+            var nowNodeTmp = _postNode;
+            _postNode = NowNode;
+            NowNode = nowNodeTmp;
+        }
+
+        #endregion
+    }
+}
---
+++ No3_Yjiromeguri/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Yjiromeguri
+{
+    // Y字路巡り 〜 横へな 2012.9.7
+    // http://nabetani.sakura.ne.jp/hena/ord3ynode/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var testCase in TestCases)
+            {
+                var input = testCase.Key;
+                var expected = testCase.Value;
+
+                var result = Play(input);
+                if (result == expected)
+                {
+                    Console.WriteLine("Success! (input = \"{0}\", result = \"{1}\")", input, result);
+                }
+                else
+                {
+                    Console.WriteLine("Failed! (input = \"{0}\", result = \"{1}\", expected = \"{2}\")", input, result, expected);
+                }
+            }
+
+            Console.WriteLine("60[s]後に画面を閉じます。");
+            Thread.Sleep(60000);
+        }
+
+
+        static public string Play(string input)
+        {
+            //初期値
+            var moveMng = new MovementManager('B', 'A');
+            var output = new StringBuilder();
+            output.Append('A');
+
+            foreach (var command in input)
+            {
+                if (command == 'r')
+                {
+                    moveMng.TurnRight();
+                }
+                else if(command == 'l')
+                {
+                    moveMng.TurnLeft();
+                }
+                else if(command == 'b')
+                {
+                    moveMng.Back();
+                }
+                else
+                {
+                    return string.Format("無効な文字\'{0}\'が含まれています", command);
+                }
+                output.Append(moveMng.NowNode);
+            }
+
+            return output.ToString();
+        }
+
+        #region テストケース
+
+        static Dictionary<string, string> TestCases = new Dictionary<string, string>()
+        {
+            /*0*/ {"b", "AB" },
+            /*1*/ {"l", "AD" },
+            /*2*/ {"r", "AC" },
+            /*3*/ {"bbb", "ABAB" },
+            /*4*/ {"rrr", "ACBA" },
+            /*5*/ {"blll", "ABCAB" },
+            /*6*/ {"llll", "ADEBA" },
+            /*7*/ {"rbrl", "ACADE" },
+            /*8*/ {"brrrr", "ABEDAB" },
+            /*9*/ {"llrrr", "ADEFDE" },
+            /*10*/ {"lrlll", "ADFEDF" },
+            /*11*/ {"lrrrr", "ADFCAD" },
+            /*12*/ {"rllll", "ACFDAC" },
+            /*13*/ {"blrrrr", "ABCFEBC" },
+            /*14*/ {"brllll", "ABEFCBE" },
+            /*15*/ {"bbrllrrr", "ABACFDEFD" },
+            /*16*/ {"rrrrblll", "ACBACABCA" },
+            /*17*/ {"llrlrrbrb", "ADEFCADABA" },
+            /*18*/ {"rrrbrllrr", "ACBABEFCAD" },
+            /*19*/ {"llrllblrll", "ADEFCBCADEB" },
+            /*20*/ {"lrrlllrbrl", "ADFCBEFDFCB" },
+            /*21*/ {"lllrbrrlbrl", "ADEBCBACFCAB" },
+            /*22*/ {"rrrrrrlrbrl", "ACBACBADFDEB" },
+            /*23*/ {"lbrbbrbrbbrr", "ADABABEBCBCFE" },
+            /*24*/ {"rrrrlbrblllr", "ACBACFCACFDAB" },
+            /*25*/ {"lbbrblrlrlbll", "ADADFDABCFDFED" },
+            /*26*/ {"rrbbrlrlrblrl", "ACBCBADFEBEFDA" },
+            /*27*/ {"blrllblbrrrrll", "ABCFDADEDABEDFE" },
+            /*28*/ {"blrllrlbllrrbr", "ABCFDABCBEFDEDA" },
+            /*29*/ {"lbrbbrllllrblrr", "ADABABEFCBEDEBCF" },
+            /*30*/ {"rrrrbllrlrbrbrr", "ACBACABCFDEDADFC" },
+        };
+
+        #endregion
+
+    }
+}
